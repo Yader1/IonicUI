fb6c6f9f778233c996565843ba6980e957e0691e
Yader Campbell <yadermejiac123@gmail.com>
2021-04-09 10:49:48 -0600

    UI inicio

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -18,6 +18,41 @@ export class HomePage {
       icon:"alert-circle-outline",
       name:"Alerta",
       redirecTo:"/alerta"
+    },
+    {
+      icon:"people-outline",
+      name:"Avatar",
+      redirecTo:"/avatar"
+    },
+    {
+      icon:"shapes-outline",
+      name:"Botones Flotantes",
+      redirecTo:"/botonfloat"
+    },
+    {
+      icon:"stop-outline",
+      name:"Botones",
+      redirecTo:"/botons"
+    },
+    {
+      icon:"checkbox-outline",
+      name:"Check Box",
+      redirecTo:"/checkbox"
+    },
+    {
+      icon:"card-outline",
+      name:"Tarjetas",
+      redirecTo:"/tarjetas"
+    },
+    {
+      icon:"today-outline",
+      name:"Data Time",
+      redirecTo:"/datatime"
+    },
+    {
+      icon:"reorder-four-outline",
+      name:"Item",
+      redirecTo:"/item"
     }
   ]
 
